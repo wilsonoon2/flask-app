ffc78271555cc48d04bd20d25f43d9bea5252473
wilsonoon2 <wilson.oontb@outlook.com>
2026-01-27 16:43:11 +0800

    add python code

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__) 
 @app.route("/") 
 def hello_world(): 
-    return "<p>Hello, Jazeel!</p>" 
+    return "<p>Hello, Wilson!</p>" 
 if __name__ == '__main__': 
     app.run(host="0.0.0.0", port=8080)
